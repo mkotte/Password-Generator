d95d4f10cba30b96c47969736bb7408be6ce954c
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-13 13:31:11 -0400

    psuedocode for js

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
+// Add arrays of possible password variables
+
+// Add values for password options
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,5 +15,11 @@ function writePassword() {
 }
 
 
+// Create generatePassword function using values from modal
+  // conditional statement for checkbox values
+  // value of desired length
+  // math.floor(math.random) for random password values
+
+//Add event listener for modal submit
 
 function generatePassword();
