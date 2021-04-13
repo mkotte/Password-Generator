bb86834c498f54fe311d8af875cca9a0fc2a5a23
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-13 15:13:13 -0400

    checkboxes evaluations working

--- develop/script.js
+++ develop/script.js
@@ -10,11 +10,13 @@ const characters = {
 }
 
 // Values for password options on modal
-const lowercaseOpt = document.getElementById("lowercaseOpt");
+const lowercaseOpt = document.getElementById("lowercaseOpt"); 
 const uppercaseOpt = document.getElementById("uppercaseOpt");
 const numericOpt = document.getElementById("numericOpt");
 const specialOpt = document.getElementById("specialOpt");
 const passwordLength = document.getElementById("password-length");
+const modalSubmit = document.getElementById("modalSubmit");
+console.log(lowercaseOpt && true)
 
 // Write password to the #password input
 function writePassword() {
@@ -29,17 +31,31 @@ function writePassword() {
 // Create generatePassword function using values from modal
 function generatePassword(){
   let valuesSelected = [];
-  // conditional statement for checkbox values
+  // conditional statement's for checkbox values
+  if (lowercaseOpt.checked){
+    valuesSelected += characters.lowercase
+  }
+  if (uppercaseOpt.checked){
+    valuesSelected += characters.uppercase
+  }
+  if (numericOpt.checked){
+    valuesSelected += characters.numeric
+  }
+  if (specialOpt.checked){
+    valuesSelected += characters.special
+  }
+
+  console.log(valuesSelected)
+
+  
+
   // value of desired length
-  // math.floor(math.random) for random password values
+  // math.floor(math.random()) for random password values
 
 
 
 
 }
 
-  
-
 //Add event listener for modal submit
-
-function generatePassword();
+modalSubmit.addEventListener('click', generatePassword)
