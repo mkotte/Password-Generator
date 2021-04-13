9d789d4a7313405e69e890e009864d3c28928318
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-13 12:23:59 -0400

    adding modals w/ forms

--- develop/script.js
+++ develop/script.js
@@ -7,8 +7,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-   
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword();
